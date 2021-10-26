4dae2968dd18d319fdf8068154ad8f1621944143
Sborges17 <1191053@isep.ipp.pt>
2021-10-26 22:15:19 +0100

    <G020-16> atualizações no esqueleto do projeto

---
+++ Domain/Users/Emotion.cs
@@ -0,0 +1,8 @@
+namespace DDDNetCore.Domain.Users{
+    public enum Emotion {
+        sadness,
+        happiness,
+        anger,
+        stress
+    }
+}
--- Domain/Users/EmotionalState.cs
+++ Domain/Users/EmotionalState.cs
@@ -1,8 +1,14 @@
+using DDDNetCore.Domain.Shared;
+
 namespace DDDNetCore.Domain.Users
 {
-    public class EmotionalState{
-        private string name {get; set;}
+    public class EmotionalState : IValueObject{
+        private Emotion emotion {get; set;}
 
         public EmotionalState(){}
+
+        public EmotionalState(Emotion emotion){
+            this.emotion = emotion;
+        }
     }
 }
