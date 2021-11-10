21d153a0628676360470c9fbe7ea4d954279acb0
1190775 <1190775@isep.ipp.pt>
2021-11-10 23:00:58 +0000

    < G020-14 > Introduction Status update

---
+++ MasterDataSocialNetwork/Domain/Introductions/IntroductionStatus.cs
@@ -0,0 +1,14 @@
+using System;
+using DDDNetCore.Domain.Shared;
+
+namespace DDDNetCore.Domain.Introductions
+{
+    public enum IntroductionDecision
+     {
+        PENDING_APPROVAL=0,
+        APPROVAL_ACCEPTED=1,
+        APPROVAL_DECLINED=2,
+        ACCEPTED,
+        DECLINED
+     }
+}
