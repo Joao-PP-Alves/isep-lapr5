88707cdb48568a4b599a4ae1f49deb2fc8e8ba22
inescesar <1171426@isep.ipp.pt>
2021-11-24 19:42:31 +0000

    <G020-39> funcionalidade implementada

--- ArtificialIntelligence/XTagsParam.pl
+++ ArtificialIntelligence/XTagsParam.pl
@@ -1,4 +1,28 @@
-%receber X
+%getUsersWithXTagsParam 
+
+getUsersWithXTagsParam(Param,ListaTags,ListaCombTags,ListaUserTags,ListaUsers,Result):-
+    length(ListaTags,Q),
+    Param =< Q,
+    todas_combinacoes(Param,ListaTags,ListaCombTags),
+    listarTodosUsers(ListaUserTags,ListaUsers),
+  %  writeln(ListaUserTags),
+    x(ListaCombTags, ListaCombTags, ListaUserTags, ListaUsers, Result).
+    
+
+x([], _,_,_,[]):-!.
+x(_, _,_,[],[]):-!.
+x(Combs, [],[UTs|LUTs], [U|Us], Rs):-!,
+    x(Combs, Combs, LUTs, Us, Rs).
+x(Combs, _, [], [U|Us], Rs):-!,
+    x(Combs, Combs, Us, Us, Rs).
+x(Combs, [C|Cs], [[UTs|_]|LUTs], [U|Us], [U|Rs]):-
+    %write(C),write(' -> '), writeln(UTs),nl,nl,
+    intersecaoListas(C,UTs,C),!, 
+    x(Combs, Combs, LUTs, Us, Rs).
+x(Combs, [C|Cs], LUTs, Users, Rs):-
+    x(Combs, Cs, LUTs, Users, Rs).
+    
+
 
 %verificar se o nr de tags do user é > ou = que X
 
@@ -22,13 +46,18 @@ xCombinacoesTags(X,[_|L],T):-xCombinacoesTags(X,L,T).
 
 %traçar as combinacoes com as listas de tags dos users
 
-listarTodosUsers([],[]).
-listarTodosUsers(U,LT):-findall(no(U,_,LT)).
+listarTodosUsers(L,U):-findall(User,(no(_,User,_)),U),
+    listarTodosUsers1(L,U).
+listarTodosUsers1([],[]):-!.
+listarTodosUsers1([LTags|L],[U|Us]):-findall(Tags,(no(_,U,Tags)),LTags),
+    listarTodosUsers1(L,Us).
+
+
 
 intersecaoListas([],_,[]). 
 intersecaoListas([X|L1],L2,[X|Result]):-member(X,L2),!,intersecaoListas(L1,L2,Result).
 intersecaoListas([_|L1],L2,Result):-intersecaoListas(L1,L2,Result).
 
 %se false, apresentar mensagem de erro sugestiva
-
+  
 %se true, devolver lista de users por ordem de match
