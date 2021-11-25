b9d22deda046f202d3cd0514969b078ce411fbcf
inescesar <1171426@isep.ipp.pt>
2021-11-25 09:09:28 +0000

    <G020-39> uc concluida

--- ArtificialIntelligence/XTagsParam.pl
+++ ArtificialIntelligence/XTagsParam.pl
@@ -1,40 +1,12 @@
 %getUsersWithXTagsParam 
 
 getUsersWithXTagsParam(Param,ListaTags,ListaCombTags,ListaUserTags,ListaUsers,Result):-
-    length(ListaTags,Q),
-    Param =< Q,
-    todas_combinacoes(Param,ListaTags,ListaCombTags),
-    listarTodosUsers(ListaUserTags,ListaUsers),
-  %  writeln(ListaUserTags),
-    x(ListaCombTags, ListaCombTags, ListaUserTags, ListaUsers, Result).
+    length(ListaTags,Q),                                                                    % verifica o tamanho da lista de tags
+    Param =< Q,                                                                             % verifica se o tamanho é > ou = que Param
+    todas_combinacoes(Param,ListaTags,ListaCombTags),                                       % devolve uma lista com todas as combinações possiveis
+    listarTodosUsers(ListaUserTags,ListaUsers),                                             % devolve a listagem dos users e suas tags
+    x(ListaCombTags, ListaCombTags, ListaUserTags, ListaUsers, Result).                     % devolve users que tenham X tags em comum
     
-
-x([], _,_,_,[]):-!.
-x(_, _,_,[],[]):-!.
-x(Combs, [],[UTs|LUTs], [U|Us], Rs):-!,
-    x(Combs, Combs, LUTs, Us, Rs).
-x(Combs, _, [], [U|Us], Rs):-!,
-    x(Combs, Combs, Us, Us, Rs).
-x(Combs, [C|Cs], [[UTs|_]|LUTs], [U|Us], [U|Rs]):-
-    %write(C),write(' -> '), writeln(UTs),nl,nl,
-    intersecaoListas(C,UTs,C),!, 
-    x(Combs, Combs, LUTs, Us, Rs).
-x(Combs, [C|Cs], LUTs, Users, Rs):-
-    x(Combs, Cs, LUTs, Users, Rs).
-    
-
-
-%verificar se o nr de tags do user é > ou = que X
-
-qntdTags([],0).
-qntdTags([_|T],N):-qntdTags(T,N1),(N is N1 + 1).
-
-maiorOuIgual(0,0).
-maiorOuIgual(S,X):-S>=X.
-
-%se false, permitir alteracao do X com mensagem sugestiva
-
-%se true, combinacoes de X elementos de tags
 
 todas_combinacoes(X,LTags,LcombXTags):-findall(L,xCombinacoesTags(X,LTags,L),LcombXTags).
 
@@ -42,9 +14,6 @@ xCombinacoesTags(0,_,[]):-!.
 xCombinacoesTags(X,[Tag|L],[Tag|T]):-X1 is X-1,xCombinacoesTags(X1,L,T).
 xCombinacoesTags(X,[_|L],T):-xCombinacoesTags(X,L,T).
 
-%isolar users e suas tags
-
-%traçar as combinacoes com as listas de tags dos users
 
 listarTodosUsers(L,U):-findall(User,(no(_,User,_)),U),
     listarTodosUsers1(L,U).
@@ -53,11 +22,19 @@ listarTodosUsers1([LTags|L],[U|Us]):-findall(Tags,(no(_,U,Tags)),LTags),
     listarTodosUsers1(L,Us).
 
 
-
 intersecaoListas([],_,[]). 
 intersecaoListas([X|L1],L2,[X|Result]):-member(X,L2),!,intersecaoListas(L1,L2,Result).
 intersecaoListas([_|L1],L2,Result):-intersecaoListas(L1,L2,Result).
 
-%se false, apresentar mensagem de erro sugestiva
-  
-%se true, devolver lista de users por ordem de match
+
+x([], _,_,_,[]):-!.
+x(_, _,_,[],[]):-!.
+x(Combs, [],[UTs|LUTs], [U|Us], Rs):-!,
+    x(Combs, Combs, LUTs, Us, Rs).
+x(Combs, _, [], [U|Us], Rs):-!,
+    x(Combs, Combs, Us, Us, Rs).
+x(Combs, [C|Cs], [[UTs|_]|LUTs], [U|Us], [U|Rs]):-
+    intersecaoListas(C,UTs,C),!, 
+    x(Combs, Combs, LUTs, Us, Rs).
+x(Combs, [C|Cs], LUTs, Users, Rs):-
+    x(Combs, Cs, LUTs, Users, Rs).
