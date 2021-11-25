ecc2e00f2c4335a4c2feeac6d187fd0e482a79ba
1190743 <1190743@isep.ipp.pt>
2021-11-25 10:52:09 +0000

    <G020-74> Remoção do repositório e controller da frienship, por não ser entity root + update na EntityTypeCondiguration

--- MasterDataSocialNetwork/Infrastructure/Users/FriendshipEntityTypeConfiguration.cs
+++ MasterDataSocialNetwork/Infrastructure/Users/FriendshipEntityTypeConfiguration.cs
@@ -9,8 +9,6 @@ namespace DDDNetCore.Infrastructure.Users
         public void Configure(EntityTypeBuilder<Friendship> builder)
         {
             builder.HasKey(f => new { f.Id, f.requester});
-            //builder.OwnsOne(b => b.friend);
-            //builder.OwnsOne(b => b.requester);
             builder.OwnsOne(b => b.friendshipTag);
             builder.OwnsOne(b => b.connection_strength);
             builder.OwnsOne(b => b.relationship_strength);
