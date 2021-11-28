7f0a2480b12c7a2338f84025b4cc8138c358093d
Sborges17 <1191053@isep.ipp.pt>
2021-11-28 16:45:51 +0000

    <G020-48> atualização das págunas de autenticação.

---
+++ clientapp/src/components/pages/Login.js
@@ -0,0 +1,172 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Modal from 'react-modal';
+import  {useState } from 'react';
+import userAuth from "../../hooks/UserAuth";
+import {useHistory} from "react-router-dom";
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+ function LogIn() {
+  const [show, setShow] = React.useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const [makingRequest, setMakingRequest] = useState(false);
+  const[showSignUp, setSignUp] = useState(false);
+  const [loginError, setLoginError] = useState("");
+  const {handleLogin} = userAuth();
+  const history = useHistory();
+
+  const handleSubmit = (data) => {
+    setMakingRequest(true);
+      fetch(
+        showSignUp
+          ? "https://localhost:5001/api/Users"
+          : "https://localhost:5001/api/Users/login",
+          {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            //convert react to json and send post method's body
+            body: JSON.stringify(data),
+          }
+      ).then((response) => {
+        console.log(response.status);
+        if(!response.ok) {
+          setLoginError("Wrong credentials! Try again.");
+          setMakingRequest(false);
+          return null;
+        }
+        return response.json();
+      }).then((data) => {
+        if(data) {
+          console.log(data);
+          handleLogin(data);
+          history.replace("/");
+        } else {
+          setMakingRequest(false);
+        }
+      });
+    };
+    
+    
+    /*event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };*/
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Login
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="b2">
+                  Forgot your password?
+                 </Link> 
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default LogIn;
---
+++ clientapp/src/components/pages/Signup.js
@@ -0,0 +1,200 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { FormCheck } from 'react-bootstrap';
+import { useState } from 'react';
+import PrivacyPolicy from './privacyPolicy';
+import LogIn from './Login';
+
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+//const error =
+
+const theme = createTheme();
+
+export default function SignUp() {
+    const[validated, setValidated] = useState(false);
+    const [checked, setChecked] = React.useState([true, false]);
+
+    /*const handleSubmit = (event) => {
+        const from = event.currentTarget;
+        if(form.checkValidity()===false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        setValidated(true);
+    };*/
+
+
+
+    const handleChange = (event) =>{
+      setChecked(
+        [event.currentTarget.checked, checked]
+        );
+    };
+
+    //const error = currentTarget.checked===false ;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      firstName: data.get('firstName'),
+      lastName: data.get('lastName'),
+      birthDate: data.get('birthDate'),
+      phoneNumber: data.get('phoneNumber'),
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+    
+      
+  };
+
+  return (
+
+    
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Register new account
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required={true}
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                    required
+                    fullWidth
+                    id="birthDate"
+                    label="Birth Date"
+                    name="birthDate"
+                    type="date"
+                    autocomplete="birth-date"
+                />                  
+              </Grid>    
+              <Grid item xs={12} sm ={6}>
+                <TextField
+                    required
+                    fullWidth
+                    id="phoneNumber"
+                    label="Phone number"
+                    name="phoneNumber"
+                    autoComplete="phone-number"
+                />
+              </Grid>  
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormCheck
+                  control={<Checkbox checked={checked} onChange={handleChange} color="primary" />}
+                  label="I agree with the terms and conditions."
+                  feedbackType="invalid"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  Already have an account? Login.
+                  {LogIn}
+                </Link>
+              </Grid> 
+            <Grid item>
+                <Link href="/termsConditions" variant="body2">
+                  Terms and conditions.
+                </Link>
+            </Grid> 
+            <Grid item>
+              <Link href="/privacyPolicy" variant="body2">
+                {PrivacyPolicy}
+                Privacy Policy.
+              </Link>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
