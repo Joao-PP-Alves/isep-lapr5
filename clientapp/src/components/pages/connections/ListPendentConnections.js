cb819a7b41af38a49dc4f7e8cabca3335d916f3f
1190775 <1190775@isep.ipp.pt>
2021-11-30 15:48:28 +0000

    < G020-65 > List pendent connections spa

--- clientapp/src/components/pages/connections/ListPendentConnections.js
+++ clientapp/src/components/pages/connections/ListPendentConnections.js
@@ -1,175 +1,244 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
-import Links from "../../Links";
-import AppBar from '@mui/material/AppBar';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiDrawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
+import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import SvgIcon from '@mui/material/SvgIcon';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { withStyles } from '@mui/styles';
-import { createTheme } from '@mui/material/styles';
-import TableCell from '@mui/material/TableCell';
+import Badge from '@mui/material/Badge';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { mainListItems} from '../dashboard/ListItems';
+import TableCell from '@mui/material/TableCell';
 import { AutoSizer, Column, Table } from 'react-virtualized';
+import PropTypes from 'prop-types';
+import { withStyles } from '@mui/styles';
+import clsx from 'clsx';
+import Links from "../../Links";
+import { useState, useEffect } from 'react';
 
-function HomeIcon(props) {
+
+
+function Copyright(props) {
   return (
-    <SvgIcon {...props}>
-      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-    </SvgIcon>
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
-const styles = (theme) => ({
-  flexContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    boxSizing: 'border-box',
-  },
-  table: {
-    // temporary right-to-left patch, waiting for
-    // https://github.com/bvaughn/react-virtualized/issues/454
-    '& .ReactVirtualized__Table__headerRow': {
-      ...(theme.direction === 'rtl' && {
-        paddingLeft: '0 !important',
-      }),
-      ...(theme.direction !== 'rtl' && {
-        paddingRight: undefined,
-      }),
-    },
-  },
-  tableRow: {
-    cursor: 'pointer',
-  },
-  tableRowHover: {
-    '&:hover': {
-      backgroundColor: theme.palette.grey[200],
-    },
-  },
-  tableCell: {
-    flex: 1,
-  },
-  noClick: {
-    cursor: 'initial',
-  },
-});
+const drawerWidth = 240;
 
-class MuiVirtualizedTable extends React.PureComponent {
-  static defaultProps = {
-    headerHeight: 48,
-    rowHeight: 48,
-  };
-
-  getRowClassName = ({ index }) => {
-    const { classes, onRowClick } = this.props;
-
-    return clsx(classes.tableRow, classes.flexContainer, {
-      [classes.tableRowHover]: index !== -1 && onRowClick != null,
-    });
-  };
-
-  cellRenderer = ({ cellData, columnIndex }) => {
-    const { columns, classes, rowHeight, onRowClick } = this.props;
-    return (
-      <TableCell
-        component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, {
-          [classes.noClick]: onRowClick == null,
-        })}
-        variant="body"
-        style={{ height: rowHeight }}
-        align={
-          (columnIndex != null && columns[columnIndex].numeric) || false
-            ? 'right'
-            : 'left'
-        }
-      >
-        {cellData}
-      </TableCell>
-    );
-  };
-
-  headerRenderer = ({ label, columnIndex }) => {
-    const { headerHeight, columns, classes } = this.props;
-
-    return (
-      <TableCell
-        component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, classes.noClick)}
-        variant="head"
-        style={{ height: headerHeight }}
-        align={columns[columnIndex].numeric || false ? 'right' : 'left'}
-      >
-        <span>{label}</span>
-      </TableCell>
-    );
-  };
-
-  render() {
-    const { classes, columns, rowHeight, headerHeight, ...tableProps } = this.props;
-    return (
-      <AutoSizer>
-        {({ height, width }) => (
-          <Table
-            height={height}
-            width={width}
-            rowHeight={rowHeight}
-            gridStyle={{
-              direction: 'inherit',
-            }}
-            headerHeight={headerHeight}
-            className={classes.table}
-            {...tableProps}
-            rowClassName={this.getRowClassName}
-          >
-            {columns.map(({ dataKey, ...other }, index) => {
-              return (
-                <Column
-                  key={dataKey}
-                  headerRenderer={(headerProps) =>
-                    this.headerRenderer({
-                      ...headerProps,
-                      columnIndex: index,
-                    })
-                  }
-                  className={classes.flexContainer}
-                  cellRenderer={this.cellRenderer}
-                  dataKey={dataKey}
-                  {...other}
-                />
-              );
-            })}
-          </Table>
-        )}
-      </AutoSizer>
-    );
-  }
-}
-
-MuiVirtualizedTable.propTypes = {
-  classes: PropTypes.object.isRequired,
-  columns: PropTypes.arrayOf(
-    PropTypes.shape({
-      dataKey: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      numeric: PropTypes.bool,
-      width: PropTypes.number.isRequired,
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
     }),
-  ).isRequired,
-  headerHeight: PropTypes.number,
-  onRowClick: PropTypes.func,
-  rowHeight: PropTypes.number,
-};
+  }),
+}));
 
-const defaultTheme = createTheme();
-const VirtualizedTable = withStyles(styles, { defaultTheme })(MuiVirtualizedTable);
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      boxSizing: 'border-box',
+      ...(!open && {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      }),
+    },
+  }),
+);
 
-// ---
+const mdTheme = createTheme();
 
-export default function ListPendentConnections() {
+const styles = (theme) => ({
+    flexContainer: {
+      display: 'flex',
+      alignItems: 'center',
+      boxSizing: 'border-box',
+    },
+    table: {
+      // temporary right-to-left patch, waiting for
+      // https://github.com/bvaughn/react-virtualized/issues/454
+      '& .ReactVirtualized__Table__headerRow': {
+        ...(theme.direction === 'rtl' && {
+          paddingLeft: '0 !important',
+        }),
+        ...(theme.direction !== 'rtl' && {
+          paddingRight: undefined,
+        }),
+      },
+    },
+    tableRow: {
+      cursor: 'pointer',
+    },
+    tableRowHover: {
+      '&:hover': {
+        backgroundColor: theme.palette.grey[200],
+      },
+    },
+    tableCell: {
+      flex: 1,
+    },
+    noClick: {
+      cursor: 'initial',
+    },
+  });
+  
+  class MuiVirtualizedTable extends React.PureComponent {
+    static defaultProps = {
+      headerHeight: 48,
+      rowHeight: 48,
+    };
+  
+    getRowClassName = ({ index }) => {
+      const { classes, onRowClick } = this.props;
+  
+      return clsx(classes.tableRow, classes.flexContainer, {
+        [classes.tableRowHover]: index !== -1 && onRowClick != null,
+      });
+    };
+  
+    cellRenderer = ({ cellData, columnIndex }) => {
+      const { columns, classes, rowHeight, onRowClick } = this.props;
+      return (
+        <TableCell
+          component="div"
+          className={clsx(classes.tableCell, classes.flexContainer, {
+            [classes.noClick]: onRowClick == null,
+          })}
+          variant="body"
+          style={{ height: rowHeight }}
+          align={
+            (columnIndex != null && columns[columnIndex].numeric) || false
+              ? 'right'
+              : 'left'
+          }
+        >
+          {cellData}
+        </TableCell>
+      );
+    };
+  
+    headerRenderer = ({ label, columnIndex }) => {
+      const { headerHeight, columns, classes } = this.props;
+  
+      return (
+        <TableCell
+          component="div"
+          className={clsx(classes.tableCell, classes.flexContainer, classes.noClick)}
+          variant="head"
+          style={{ height: headerHeight }}
+          align={columns[columnIndex].numeric || false ? 'right' : 'left'}
+        >
+          <span>{label}</span>
+        </TableCell>
+      );
+    };
+  
+    render() {
+      const { classes, columns, rowHeight, headerHeight, ...tableProps } = this.props;
+      return (
+        <AutoSizer>
+          {({ height, width }) => (
+            <Table
+              height={height}
+              width={width}
+              rowHeight={rowHeight}
+              gridStyle={{
+                direction: 'inherit',
+              }}
+              headerHeight={headerHeight}
+              className={classes.table}
+              {...tableProps}
+              rowClassName={this.getRowClassName}
+            >
+              {columns.map(({ dataKey, ...other }, index) => {
+                return (
+                  <Column
+                    key={dataKey}
+                    headerRenderer={(headerProps) =>
+                      this.headerRenderer({
+                        ...headerProps,
+                        columnIndex: index,
+                      })
+                    }
+                    className={classes.flexContainer}
+                    cellRenderer={this.cellRenderer}
+                    dataKey={dataKey}
+                    {...other}
+                  />
+                );
+              })}
+            </Table>
+          )}
+        </AutoSizer>
+      );
+    }
+  }
+  
+  MuiVirtualizedTable.propTypes = {
+    classes: PropTypes.object.isRequired,
+    columns: PropTypes.arrayOf(
+      PropTypes.shape({
+        dataKey: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        numeric: PropTypes.bool,
+        width: PropTypes.number.isRequired,
+      }),
+    ).isRequired,
+    headerHeight: PropTypes.number,
+    onRowClick: PropTypes.func,
+    rowHeight: PropTypes.number,
+  };
+  
+  const defaultTheme = createTheme();
+  const VirtualizedTable = withStyles(styles, { defaultTheme })(MuiVirtualizedTable);
+  
+
+function EditProfileContent() {
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
 
   useEffect(() => {
     search();
@@ -181,7 +250,7 @@ export default function ListPendentConnections() {
     fetchPendentConnections();
   }
 
-  const fetchPendentConnections = async () => {
+  const fetchPendentConnections= async () => {
 
     const data = await fetch(
       Links.MDR_URL() + "/api/connections/pendent/6257be06-d34d-4aad-a5dd-88f617da91f9"
@@ -199,7 +268,7 @@ export default function ListPendentConnections() {
   for (var i = 0; i < searchedVS.length; i++){
     var obj = searchedVS[i];
     console.log(obj);
-    sample.push([obj.id,obj.requester.value,"a"]);
+    sample.push([obj.id,obj.requester.value,obj.description.text]);
   }
 
   function createData(number, id, requester, description) {
@@ -214,19 +283,88 @@ export default function ListPendentConnections() {
     rows.push(createData(i, ...sample[i]));
   }
 
+  //TODO ia aqui!
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <HomeIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            List of Pendent Connections
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Paper style={{ height: 700, width: '100%' }}>
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
+            sx={{
+              pr: '24px', // keep right padding when drawer closed
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: '36px',
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Pendent Connections
+            </Typography>
+            <IconButton color="inherit">
+              
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              px: [1],
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List>{mainListItems}</List>
+          <Divider />
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            
+              {/* Recent Deposits */}
+              <Grid item xs={12} md={4} lg={3}>
+              <Paper sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 600,
+                    width: 1150,
+                  }}>
         <VirtualizedTable
           rowCount={rows.length}
           rowGetter={({ index }) => rows[index]}
@@ -249,9 +387,15 @@ export default function ListPendentConnections() {
           ]}
         />
       </Paper>
-    </Box>
-
+              </Grid>
+            <Copyright sx={{ pt: 4 }} />
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
-
+export default function EditProfile() {
+  return <EditProfileContent />;
+}
