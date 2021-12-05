2d2f29b6a9dac60e1e37562eed634c3bfdc87709
inescesar <1171426@isep.ipp.pt>
2021-12-05 23:00:43 +0000

    <G020-44> redes de topologia star e anel adicionadas

---
+++ ArtificialIntelligence/rede_topologia_star.pl
@@ -0,0 +1,17 @@
+no(1,ana,[natureza,pintura,musica,sw,porto]).
+no(11,antonio,[natureza,pintura,carros,futebol,lisboa]).
+no(12,beatriz,[natureza,musica,carros,porto,moda]).
+no(21,eduardo,[natureza,cinema,teatro,carros,coimbra]).
+no(22,isabel,[natureza,musica,porto,lisboa,cinema]).
+no(200,sara,[natureza,moda,musica,sw,coimbra]).
+
+ligacao(1,11,7).
+ligacao(11,1,7).
+ligacao(1,12,8).
+ligacao(12,1,6).
+ligacao(1,21,2).
+ligacao(21,1,5).
+ligacao(1,22,9).
+ligacao(22,1,3).
+ligacao(1,200,1).
+ligacao(200,1,3).
