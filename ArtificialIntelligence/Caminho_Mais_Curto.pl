0ab609a6b8bb684d46ff68de1689ccb420190247
inescesar <1171426@isep.ipp.pt>
2021-12-15 09:33:51 +0000

    <G020-115> refactor do predicado do caminho mais curto concluido

--- ArtificialIntelligence/Caminho_Mais_Curto.pl
+++ ArtificialIntelligence/Caminho_Mais_Curto.pl
@@ -19,9 +19,8 @@ one_dfs(Orig, Dest, Cam) :- dfs(Orig, Dest, Cam), !.
 dfs(Orig,Dest,Cam):-dfs2(Orig,Dest,[Orig],Cam).
 
 dfs2(Dest,Dest,LA,Cam):-!,reverse(LA,Cam).
-dfs2(Act,Dest,LA,Cam):-no(NAct,Act,_),ligacao(NAct,NX,_,_),
-    no(NX,X,_),\+ member(X,LA),dfs2(X,Dest,[X|LA],Cam).
 
+dfs2(Act,Dest,LA,Cam):-melhor_sol_minlig(_,C),length(LA,D),D<C,no(NAct,Act,_),ligacao(NAct,NX,_,_),no(NX,X,_),\+member(X,LA),dfs2(X,Dest,[X|LA],Cam).
 
 plan_minlig(Orig,Dest,LCaminho_minlig,N):-
 		get_time(Ti),
