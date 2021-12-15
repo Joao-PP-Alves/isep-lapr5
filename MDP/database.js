ab56d80a1e1b4fd99aa7d217e45ba5a933e16cde
inescesar <1171426@isep.ipp.pt>
2021-12-15 08:54:01 +0000

    <G020-81> criação da base de dados mongodb e introducao da mesma na master data

--- MDP/database.js
+++ MDP/database.js
@@ -1,12 +1,14 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const server = 'server.mlab.com:21234'; // Database server address
 const database = 'theDatabase'; // Database name
+
+
 class Database {
 constructor() {
 this._connect()
 }
-_connect() {
-mongoose.connect(`mongodb://${server}/${database}`, { useMongoClient: true });
+async _connect() {
+mongoose.connect(`mongodb://vs330.dei.isep.ipp.pt/admin`, { useMongoClient: true });
 then(() => {console.log('Database connection successful');})
 .catch(err => {console.error('Database connection error');})
 }
