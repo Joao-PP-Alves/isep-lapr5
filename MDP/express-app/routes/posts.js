29e10316c53a2cb8c92c279b0d18f20b8401f78f
Sborges17 <1191053@isep.ipp.pt>
2021-12-19 12:22:09 +0000

    < G020-93 > atualizações nos métodos para afzer posts

--- MDP/express-app/routes/posts.js
+++ MDP/express-app/routes/posts.js
@@ -4,108 +4,49 @@ const Post = require('../domain/post');
 const Joi = require('joi')
 
 //ROUTES
-router.get("/add-posts", (req, res) => {
-	//res.send("We are on posts");
-	const post = new Post({
-		id: '1',
-		name: 'post1',
-		content: 'Bom dia! #educados_respondem',
-		userId: '1'
-		//date: '18-12-2021'
-	});
 
-	post.save()
-		.then((result) => {
-			res.send(result)
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-});
-
-router.get("/", (req, res) => {
-	res.send(posts);
-});
-
-/*router.get("/:id", (req, res) => {
-	const post = posts.find((c) => c.id === parseInt(req.params.id));
-
-	if (!post) res.status(404).send("The post with the given id was not found.");
-	res.send(post);
-});*/
-
-router.post("/add-posts", (req, res) => {
-	const { error } = validatePost(req.body);
-
-	if (error) {
-		res.status(404).send(error.details[0].message);
-		return;
-	}
-
-	const post = {
-		//id: posts.length + 1,
-		name: req.body.name,
-	};
-	//posts.push(post);
-	res.send(post);
-});
-
-/*router.put("/api/Posts/:id", (req, res) => {
-	const post = posts.find((c) => c.id === parseInt(req.params.id));
-	if (!post) res.status(404).send("The post with the given id was not found.");
-
-	const { error } = validatePost(req.body);
-
-	if (error) {
-		res.status(404).send(error.details[0].message);
-		return;
-	}
-
-	post.name = req.body.name;
-	res.send(post);
-});*/
-
-function validatePost(post) {
-	const schema = {
-		name: Joi.string().required(),
-	};
-	return Joi.validate(post, schema);
-}
-
-
-/*router.post("/", (req, res) => {
-	const post = new Post({
-		//id: String,
-		name: req.body.name,
-		//content: req.body.content,
-		//userId: req.body.userId,
-		//date: req.body.date,
-	});
-
-	post .save()
-	.then(data => {
-		res.json(data);
-	})
-	.catch(err => {
+//Gets all posts
+router.get("/", async (req, res) => {
+	try{
+	const posts = await Post.find();	
+	res.json(posts);
+	}catch(err){
 		res.json({message: err});
-	})*/
-	/*const { error } = validatePost(req.body);
-
-	if (error) {
-		res.status(404).send(error.details[0].message);
-		return;
 	}
+});
 
-	const post = {
-		id: posts.length + 1,
-		name: req.body.name,
-	};
-	posts.push(post);
-	res.send(post);*/
-
-
-router.put("/:id", (req, res) => {
+//Gets a single post
+//ainda falta pôr bem
+router.get("/:id", async (req, res) => {
+	const posts = await Post.find();
 	const post = posts.find((c) => c.id === parseInt(req.params.id));
+
+	if (!post) res.status(404).send("The post with the given id was not found.");
+	res.send(post);
+});
+
+
+//Submits a post
+router.post("/", async (req, res) => {
+	const post = new Post({
+		name: req.body.name,
+		content: req.body.content,
+		userId: req.body.userId
+	});
+	try{
+	const savedPost = await post.save();
+	res.json(savedPost);
+	} catch(err){
+		res.json({ message: err});
+	}
+});
+
+
+//Changes the information of a post
+//ainda falta corrigir
+router.put("/:_id", async (req, res) => {
+	const posts = await Post.find();
+	const post = posts.find((c) => c._id === parseInt(req.params._id));
 	if (!post) res.status(404).send("The post with the given id was not found.");
 
 	const { error } = validatePost(req.body);
@@ -119,6 +60,8 @@ router.put("/:id", (req, res) => {
 	res.send(post);
 });
 
+
+//validates post information
 function validatePost(post) {
 	const schema = {
 		name: Joi.string().required(),
